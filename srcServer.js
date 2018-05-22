2b74a1cdebf2925226f1cbb07218558047f62271
Watts <hbwatts@spotted-tang.local>
2018-05-22 13:48:27 -0700

    module nine work in progress

---
+++ srcServer.js
@@ -0,0 +1,32 @@
+var express = require('express');
+//import express from 'express';
+var path = require('path');
+var open = require('open');
+const webpack=require('webpack');
+var config=require('../webpack.config.dev');
+
+const port = 3000;
+const app = express();
+const compiler=webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo:true,
+    publicPath: config.output.publicPath
+}));
+
+
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+app.listen(port, function(err){
+
+if (err){
+    console.log(err);
+}
+else{
+open('http://localhost:' + port);
+
+}
+
+});
